8654604e6a0fa624e751673470861eda4c5a8338
robsonmars <robsonmarinho1@gmail.com>
2022-10-26 09:42:04 -0300

    Exercicios Python

---
+++ Informatica_Aplicada/src/Ex15_ReadFiles.py
@@ -0,0 +1,19 @@
+#Reading Files 
+# You know how to get input from a user with input or argv. Now you will learn about reading from a file. 
+#  Working with files is an easy way to erase your work if you are not careful
+# 
+# The second file isn’t a script but a plain text file we’ll be reading in our script. 
+# See the file: Ex15_ReadThis.txt
+
+from sys import argv 
+script, filename = argv 
+txt = open(filename)
+print(f"Here's your file {filename}:") 
+print(txt.read())
+print("Type the filename again:") 
+file_again = input("> ")
+txt_again = open(file_again) 
+print(txt_again.read())
+
+#I made a file called Ex15_ReadThis.txt and ran my script, using:
+# $ python3.8 Ex15_ReadFiles.py Ex15_ReadThis.txt
---
+++ Informatica_Aplicada/src/ExMathPi.py
@@ -0,0 +1,13 @@
+import math
+
+x = math.pi
+
+print(x)
+
+#duas casas decimais 
+# A função round() recebe 2 argumentos. O primeiro é o número que deseja arredondar, e o segundo é a quantidade de casas decimais que você deseja.
+
+
+x = round(x, 2)
+
+print(f"duas casas decimais: {x}")
