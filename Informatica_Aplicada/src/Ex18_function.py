18574539a59ee9a1e09f74cea2d77444dd72b50c
robsonmars <robsonmarinho1@gmail.com>
2022-10-28 15:06:50 -0300

    criado ex19_function_var

--- Informatica_Aplicada/src/Ex18_function.py
+++ Informatica_Aplicada/src/Ex18_function.py
@@ -1,18 +1,18 @@
 # this one is like your scripts with argv
-def print_two(*args):
+def nome_funcao_two(*args):
     arg1, arg2 = args
-    print(f"arg1: {arg1}, arg2: {arg2}")
+    print(f"argumento 1: {arg1}, argumento 2: {arg2}")
 # ok, that *args is actually pointless, we can just do this
-def print_two_again(arg1, arg2):
-    print(f"arg1: {arg1}, arg2: {arg2}")
+def nome_funcao_two_again(arg1, arg2):
+    print(f"argumento 1: {arg1}, argumento 2: {arg2}")
 # this just takes one argument
-def print_one(arg1):
-    print(f"arg1: {arg1}")
+def nome_funcao_one(arg1):
+    print(f"argumento: {arg1}")
 # this one takes no arguments
-def print_none():
+def nome_funcao_none():
     print("I got nothin'.")
 
-print_two("Fulano","Sincrano")
-print_two_again("Informática","Aplicada")
-print_one("Engenharia!")
-print_none()
+nome_funcao_two("Robson","Marinho")
+nome_funcao_two_again("Informática","Aplicada")
+nome_funcao_one("Engenharia!")
+nome_funcao_none()
