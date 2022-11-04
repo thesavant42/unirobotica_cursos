cbf367d88a321eb4804da527ecac474760070016
robsonmars <robsonmarinho1@gmail.com>
2022-11-04 14:17:12 -0300

    Aula_funcao_file_RedesPetri

---
+++ Informatica_Aplicada/src/Conventions_Encondings.py
@@ -0,0 +1,8 @@
+#The most popular convention ended up being American Standard Code for Information Interchange or ASCII. 
+# This standard maps a number to a letter. The number 90 is Z, which in bits is 1011010
+
+print(0b1011010)
+
+print(ord('Z'))
+
+print(chr(90))
