fdf9ebcaac7e20709ed19e89f41d6f835f9db282
Robson Marinho <robsonmarinho1@gmail.com>
2023-03-23 11:18:56 -0300

    incluso novos sketch

---
+++ Informatica_Aplicada/src/Asks for your name.py
@@ -0,0 +1,13 @@
+# This program asks for your name and uses
+
+# it in an output message.
+
+# Run the program here by typing "Ctrl R"
+
+ 
+
+name=input("What's your name? ")
+
+print("Hello, ", name)
+
+print("\n Press ctrl+R to run again")
