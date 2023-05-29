531008800640d283f6b173471104b9395dbf8027
robsonmars <robsonmarinho1@gmail.com>
2023-05-29 19:04:56 -0300

    aula loop, control fluxo, if, range

--- Informatica_Aplicada/src/Python Basico/Ex5_Fibo_print_.py
+++ Informatica_Aplicada/src/Python Basico/Ex5_Fibo_print_.py
@@ -21,5 +21,15 @@ a, b = 0, 1
 while a < 1000:
 
     print(a, end=',')
-
+    
     a, b = b, a+b #The keyword argument end can be used to avoid the newline after the output, or end the output with a different string:
+
+
+'''
+        Since ** has higher precedence than -, -3**2 will be interpreted as -(3**2) and thus result in -9. To avoid this and get 9, you can use (-3)**2.
+2
+
+    Unlike other languages, special characters such as \n have the same meaning with both single ('...') and double ("...") quotes. The only difference between the two is that within single quotes you don’t need to escape " (but you have to escape \') and vice versa.
+'''
+
+print("\n")
